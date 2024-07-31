c9469256e552a2ebf30813bdc6fc673f7a656dec
jdeghelshi95 <98611117+jdeghelshi95@users.noreply.github.com>
2024-07-30 22:31:06 -0400

    added hyperlink pics

--- bobashop/src/Components/Header.jsx
+++ bobashop/src/Components/Header.jsx
@@ -35,7 +35,7 @@ export default function Example() {
         <div className="relative lg:col-span-5 lg:-mr-8 xl:absolute xl:inset-0 xl:left-1/2 xl:mr-0">
           <img
             alt=""
-            src={Background}
+            src="https://tinyurl.com/boba121212"
             className="aspect-[3/2] w-full bg-gray-50 object-cover lg:absolute lg:inset-0 lg:aspect-auto lg:h-full"
           />
         </div>
